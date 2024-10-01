b47385f7325b96df4c18c2283a0157582f522498
Максим Сергеев <aions07@mail.ru>
2024-10-01 20:08:44 +1200

    add gcd gameLogic

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,28 +1,20 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions */
 
-import readlineSync from 'readline-sync';
-import getUserName, { randomNumberTo100 } from '../cli.js';
+import { getRandomNumber } from '../cli.js';
+import gameEngine from '../gameEngine.js';
 
 const findGCD = (a, b) => (b === 0 ? a : findGCD(b, a % b));
+const description = 'Find the greatest common divisor of given numbers.';
 
-const gcd = () => {
-  const userName = getUserName();
-  console.log('Find the greatest common divisor of given numbers.');
-  for (let i = 0; i < 3; i += 1) {
-    const firstRandNumber = randomNumberTo100();
-    const secondRandNumber = randomNumberTo100();
+const gameLogic = () => {
+  const firstRandNumber = getRandomNumber(100);
 
-    const correctAnswer = findGCD(firstRandNumber, secondRandNumber);
-    console.log(`Question: ${firstRandNumber} ${secondRandNumber}`);
-    const userAnswer = readlineSync.question();
+  const secondRandNumber = getRandomNumber(100);
 
-    if (correctAnswer.toString() === userAnswer) {
-      console.log(`Your answer: ${correctAnswer}\ncorrect`);
-    } else {
-      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-  return true;
+  const correctAnswer = findGCD(firstRandNumber, secondRandNumber);
+
+  return { correctAnswer, question: `Question: ${firstRandNumber} ${secondRandNumber}` };
 };
-gcd();
+
+gameEngine(description, gameLogic);
