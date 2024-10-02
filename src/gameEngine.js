56f20335a8e5c3dd95a7ad5a196233a0b85b5a16
Максим Сергеев <aions07@mail.ru>
2024-10-02 23:16:41 +1200

    fix linter

--- src/gameEngine.js
+++ src/gameEngine.js
@@ -1,27 +1,26 @@
-import getUserName from '../src/cli.js';
 import readlineSync from 'readline-sync';
+// eslint-disable-next-line import/extensions
+import getUserName from './cli.js';
 
 const gameEngine = (description, gameLogic) => {
-    const userName = getUserName();
-    console.log(description)
-    for (let i = 0; i < 3; i += 1) {
-
-
+  const userName = getUserName();
+  console.log(description);
+  for (let i = 0; i < 3; i += 1) {
     gameLogic();
-    const { correctAnswer, question } = gameLogic()
-    console.log(question)
+    const { correctAnswer, question } = gameLogic();
+    console.log(question);
     const userAnswer = readlineSync.question();
 
     if (correctAnswer.toString() === userAnswer) {
-        console.log(`Your answer: ${correctAnswer}\ncorrect`);
+      console.log(`Your answer: ${correctAnswer}\ncorrect`);
     } else {
-        return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
     }
-}
+  }
 
-    console.log(`Congratulations, ${userName}!`);
-  
-    return true;
+  console.log(`Congratulations, ${userName}!`);
+
+  return true;
 };
 
 export default gameEngine;
